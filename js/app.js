1a6e04aa3a937b1d2bf889b075fdba957f745ac4
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-02-13 17:08:47 +0530

    libraries added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,50 @@
 /**
  * Created by abhishekrathore on 2/13/17.
  */
+angular.module("forum",["ngRoute","ui.bootstrap"])
+    .config(function($routeProvider){
+
+        $routeProvider
+            .when("/",
+                {
+                    templateUrl: "partials/login.html",
+                    controller:"loginCtrl",
+                    controllerAs :"login"
+                })
+            .when("/home",
+                {
+                    templateUrl: "partials/home.html",
+                    controller:"homeCtrl",
+                    controllerAs :"home"
+                })
+
+    })
+
+    .controller("loginCtrl",loginCtrl)
+    .controller("homeCtrl",homeCtrl)
+
+
+
+
+function loginCtrl($location){
+    
+    var login = this;
+
+        login.submit = function() {
+
+            console.log(login.user);
+            $location.path("/home")
+            
+        }
+    
+} //Hoisting
+
+
+function homeCtrl(){
+
+    var home = this;
+
+
+    console.log("home ctrl")
+
+}

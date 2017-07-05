26b09f88b837e061057c6a77fc0fb635224f8810
WIN7-PC1\Diljeet <diljeet.suri@gmail.com>
2017-07-05 17:09:59 +0530

    comment section added on newfeeds

--- js/controllers/newFeeds.Ctrl.js
+++ js/controllers/newFeeds.Ctrl.js
@@ -4,6 +4,7 @@ function newFeedsCtrl($timeout, $uibModal){
     var newFeeds = this;
     newFeeds.user = Parse.User.current();
     newFeeds.posts = [];
+    newFeeds.comments =[1,2];
     newFeeds.getData = function(num){
         var Post = Parse.Object.extend("Post");
         var query = new Parse.Query(Post);
@@ -12,10 +13,34 @@ function newFeedsCtrl($timeout, $uibModal){
         query.include("user");
         query.find({
             success: function(results) {
-                $timeout(function () {
-                    console.log(results);
-                    newFeeds.posts = results;
-                },50);
+                var Com = Parse.Object.extend("Comment");
+                var query2 = new Parse.Query(Com);
+                query.include("user");
+                query.include("post");
+                 query2.find({
+                     success: function(res){
+                         for(var i = 0; i<res.length;i++){
+                             loop1:
+                             for(var j=0; j<results.length; j++){
+                                 if(results[j].comments == undefined)
+                                 results[j].comments = [];
+                                 if(results[j].id == res[i].get('post').id){
+                                     results[j].comments.push(res[i]);
+                                     console.log(j)
+                                     break loop1;
+                                 }
+                             }
+                         }
+                         $timeout(function () {
+                             console.log(results);
+                             newFeeds.posts = results;
+                         }, 50);
+                     },
+                     error: function(err){
+                         alert("err");
+                     }
+                 })
+
 
 
             },
@@ -80,6 +105,35 @@ function newFeedsCtrl($timeout, $uibModal){
                                     }
            })
         }
+    }
+
+    newFeeds.postComment = function(post){
+        if(newFeeds.newComment){
+        // var commentBlockUI = blockUI.instances.get('commentBlockUI'+post.id);
+        // commentBlockUI.start("posting comment...");
+        var Comment = Parse.Object.extend("Comment");
+            var comment = new Comment();
+
+            comment.set("user", Parse.User.current());
+            comment.set("post", post);
+            comment.set("comment", newFeeds.newComment );
+            comment.save(null, {
+                success: function(result) {
+                    $timeout(function () {
+                        post.comments.push(result);
+                        // commentBlockUI.stop();
+                        newFeeds.newComment = "";
+                    })
+                },
+                error: function(post, error) {
+                    alert('Failed to create new object, with error code: ' + error.message);
+                }
+            });
+        }
+        else{
+            alert("reply box is empty");
+        }
+     
     }
 
 }
